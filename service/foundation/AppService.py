45e17baedc94ee3d5392de090dea64bef30519b1
lsj524993618 <524993618@qq.com>
2015-05-15 14:20:13 +0800

    modified something

--- service/foundation/AppService.py
+++ service/foundation/AppService.py
@@ -4,7 +4,20 @@ import Config
 
 class TxAppService:
     def __init__(self):
-        config = Config.get_config()
-        self.mongoClient = MongoClient(config['mongohost'],config['mongoport'])
-        #self.cms = TxCms(self.mongoClient['test'], config['cmsStorages'])
-        self.fdb = TxFdb(config['fdbStorages'])
+        self.__config = Config.get_config()
+
+    def getMongoClient(self):
+        try:
+            value = self.__mongoClient
+        except:
+            self.__mongoClient = MongoClient(self.__config['mongohost'],self.__config['mongoport'])
+
+        return self.__mongoClient
+
+    def getFdb(self):
+        try:
+            value = self.__fdb
+        except:
+            self.__fdb = TxFdb(self.__config['fdbStorages'])
+
+        return self.__fdb
